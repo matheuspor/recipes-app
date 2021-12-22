650ddb8b045fe1068f8fa4d452ec75d5cf3e5102
Matheus <matheus.pormanhani@gmail.com>
2021-12-22 14:00:00 -0300

    Login page elements

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,12 +1,54 @@
-import { Typography } from '@mui/material';
-import React from 'react';
+import { Button, Container, Stack, TextField, Typography } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+
+const MIN_PASSWORD_LENGTH = 6;
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [formCheck, setFormCheck] = useState(true);
+
+  useEffect(() => {
+    if (email && (password.length >= MIN_PASSWORD_LENGTH)) {
+      setFormCheck(false);
+    } else setFormCheck(true);
+  }, [email, password]);
+
   return (
-    <div>
-      <Typography variant="h2" sx={ { textAlign: 'center' } }>
-        Login
-      </Typography>
-    </div>
+    <Container
+      component="main"
+      maxWidth="xs"
+      sx={ { my: 4 } }
+    >
+      <Stack
+        component="form"
+        spacing={ 3 }
+        sx={ { alignItems: 'center', textAlign: 'center' } }
+      >
+        <Typography variant="h2">
+          Login
+        </Typography>
+        <TextField
+          required
+          autoComplete="email"
+          type="email"
+          label="Email"
+          variant="outlined"
+          onChange={ ({ target: { value } }) => setEmail(value) }
+        />
+        <TextField
+          type="password"
+          required
+          inputProps={ { 'data-testid': 'password-input' } }
+          autoComplete="password"
+          label="Password"
+          variant="outlined"
+          onChange={ ({ target: { value } }) => setPassword(value) }
+        />
+        <Button disabled={ formCheck } variant="contained">
+          Enter
+        </Button>
+      </Stack>
+    </Container>
   );
 }
--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,9 +1,31 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
 import Login from '../pages/Login';
 
-test('renders login page', () => {
+test('Renders login page', () => {
   render(<Login />);
-  const linkElement = screen.getByRole('heading', { name: /login/i });
-  expect(linkElement).toBeInTheDocument();
+
+  const loginHeading = screen.getByRole('heading', { name: /login/i });
+  const emailInput = screen.getByRole('textbox', { name: /email/i });
+  const passwordInput = screen.getByTestId('password-input');
+  const enterButton = screen.getByRole('button', { name: /enter/i });
+
+  expect(loginHeading).toBeInTheDocument();
+  expect(emailInput).toBeInTheDocument();
+  expect(passwordInput).toBeInTheDocument();
+  expect(enterButton).toBeInTheDocument();
+});
+
+test('Test enter button', () => {
+  render(<Login />);
+  const enterButton = screen.getByRole('button', { name: /enter/i });
+  const emailInput = screen.getByRole('textbox', { name: /email/i });
+  const passwordInput = screen.getByTestId('password-input');
+  expect(enterButton).toBeDisabled();
+
+  userEvent.type(emailInput, 'test@email.com');
+  userEvent.type(passwordInput, '123456');
+
+  expect(enterButton).toBeEnabled();
 });
