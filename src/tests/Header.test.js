edf65debee0acd49b4761c8bf054206bc0cd4490
Matheus Pormanhani <matheus.pormanhani@gmail.com>
2022-06-03 15:46:29 +0000

    test: add Header tests

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -10,21 +10,40 @@ const datatestIds = {
   searchIcon: 'search-recipe-btn',
 };
 
-test('Renders Header component', async () => {
-  renderWithRouter(<Header />, { route: '/recipes-app/foods' });
-  await waitFor(() => {
-    const profileIcon = screen.getByTestId(datatestIds.profileIcon);
-    const pageTitle = screen.getByTestId(datatestIds.pageTitle);
-    const searchIcon = screen.getByTestId(datatestIds.searchIcon);
-    expect(profileIcon).toBeInTheDocument();
-    expect(searchIcon).toBeInTheDocument();
-    expect(pageTitle).toBeInTheDocument();
+describe('Tests Header component', () => {
+  describe('When route is /recipes-app/foods', () => {
+    test('Renders Header with correct title', async () => {
+      renderWithRouter(<Header />, { route: '/recipes-app/foods' });
+      await waitFor(() => {
+        const profileIcon = screen.getByTestId(datatestIds.profileIcon);
+        const pageTitle = screen.getByTestId(datatestIds.pageTitle);
+        const searchIcon = screen.getByTestId(datatestIds.searchIcon);
+        expect(profileIcon).toBeInTheDocument();
+        expect(pageTitle).toBeInTheDocument();
+        expect(pageTitle.innerHTML).toStrictEqual('foods');
+        expect(searchIcon).toBeInTheDocument();
+      });
+    });
   });
-});
-test('Tests profileIcon redirection', async () => {
-  renderWithRouter(<Header />, { route: '/recipes-app/foods' });
+  describe('When route is /recipes-app/made-recipes', () => {
+    test('Renders Header with correct title', async () => {
+      renderWithRouter(<Header />, { route: '/recipes-app/made-recipes' });
+      await waitFor(() => {
+        const profileIcon = screen.getByTestId(datatestIds.profileIcon);
+        const pageTitle = screen.getByTestId(datatestIds.pageTitle);
+        const searchIcon = screen.getByTestId(datatestIds.searchIcon);
+        expect(profileIcon).toBeInTheDocument();
+        expect(searchIcon).toBeInTheDocument();
+        expect(pageTitle.innerHTML).toStrictEqual('made recipes');
+        expect(pageTitle).toBeInTheDocument();
+      });
+    });
+  });
+  test('Tests profileIcon redirection', async () => {
+    renderWithRouter(<Header />, { route: '/recipes-app/foods' });
 
-  const profileIcon = screen.getByTestId(datatestIds.profileIcon);
-  userEvent.click(profileIcon);
-  expect(window.location.pathname).toStrictEqual('/recipes-app/profile');
+    const profileIcon = screen.getByTestId(datatestIds.profileIcon);
+    userEvent.click(profileIcon);
+    expect(window.location.pathname).toStrictEqual('/recipes-app/profile');
+  });
 });
